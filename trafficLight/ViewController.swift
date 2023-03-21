6f7f291e0f8b176d30a81843d6d38b96a20df3ea
DmKeller <dmit.keller@gmail.com>
2023-03-22 02:14:59 +0300

    animation

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -17,20 +17,12 @@ final class ViewController: UIViewController {
     
     private var colorLight: ColorLight = .red
     private let turnerOn: CGFloat = 1
-    private let turnedOff: CGFloat = 0.2
+    private let turnedOff: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         conditionButton.layer.cornerRadius = 10
-        UIView.animate(withDuration: 2.5) {
-            self.redView.center = self.view.center
-            self.redView.alpha = 0.2
-            self.yellowView.center = self.view.center
-            self.yellowView.alpha = 0.2
-            self.greenView.center = self.view.center
-            self.greenView.alpha = 0.2
-        }
- 
+        animateRed()
     }
     
     override func viewWillLayoutSubviews() {
@@ -39,6 +31,7 @@ final class ViewController: UIViewController {
         greenView.layer.cornerRadius = redView.frame.width / 2
     }
     
+
     @IBAction private func changeColorLightButton() {
         if conditionButton.currentTitle == "START" {
             conditionButton.setTitle("NEXT", for: .normal)
@@ -47,16 +40,63 @@ final class ViewController: UIViewController {
         switch  colorLight {
         case .red:
             redView.alpha = turnerOn
-            colorLight = .yellow
             greenView.alpha = turnedOff
+            colorLight = .yellow
         case .yellow:
             redView.alpha = turnedOff
             yellowView.alpha = turnerOn
             colorLight = .green
+            animateYellow()
+            //increaseYellow()
         case .green:
             yellowView.alpha = turnedOff
             greenView.alpha = turnerOn
             colorLight = .red
+            animateGreen()
+            //increaseGreen()
+        }
+    }
+    
+    // MARK AnimateFunc
+    // кнопка улетает вправо
+    func increaseRed() {
+        UIView.animate(withDuration: 0.5, delay: 0.4,
+                       options: .repeat, animations: {
+            self.redView.center.x += self.view.bounds.width
+        }, completion: nil)
+    }
+    func increaseYellow() {
+        UIView.animate(withDuration: 0.8, delay: 0.4,
+                       options: .transitionCurlUp, animations: {
+            self.yellowView.center.x += self.view.bounds.maxX
+        }, completion: nil)
+    }
+    func increaseGreen() {
+        UIView.animate(withDuration: 0.8, delay: 0.4,
+                       options: .curveEaseInOut, animations: {
+            self.greenView.center.x += self.view.center.y
+        }, completion: nil)
+    }
+    
+    // мигание
+    func animateRed() {
+        UIView.animate(withDuration: 4, delay: 0.4, options: .repeat) {
+            self.redView.contentScaleFactor += 1
+            self.redView.alpha += 0.2
+            
+        }
+    }
+    func animateYellow() {
+        UIView.animate(withDuration: 2, delay: 0.4, options: .repeat){
+            self.redView.contentScaleFactor += 1
+            self.yellowView.alpha = +0.2
+        }
+        
+    }
+    func animateGreen() {
+        UIView.animate(withDuration: 4, delay: 0.4, options: .repeat) {
+            self.redView.contentScaleFactor += 1
+            self.greenView.alpha = +0.2
         }
     }
 }
